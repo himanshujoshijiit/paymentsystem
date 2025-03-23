a58975e97554bc7209eea07a811efc376d8a11e6
himanshu <himanshujiit2018@gmail.com>
2025-03-23 11:44:26 +0530

    intial code`

---
+++ src/main/java/com/paymenthj/paymenthjsystem/PaymenthjsystemApplication.java
@@ -0,0 +1,13 @@
+package com.paymenthj.paymenthjsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymenthjsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymenthjsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymenthj/paymenthjsystem/PaymenthjsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymenthj.paymenthjsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymenthjsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
